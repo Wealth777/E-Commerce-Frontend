c93de25ba6806313733fe16cfcffb40a55e9673c
Wealth777 <olujidewealth3@gmail.com>
2026-05-26 15:05:35 +0100

    fix: update API base URL for improved backend connectivity

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:6778/api';
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://e-commerce-backend-lmdt.onrender.com';
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://new-e-commerce-backend-gwr5.onrender.com/api';
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
